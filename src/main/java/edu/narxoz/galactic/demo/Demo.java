917b4a96d7ef0ed438d548b0d0596f656b5d39db
aruzhan <a.kinayatova@narxoz.kz>
2026-02-02 10:46:29 +0500

    full 4

---
+++ src/main/java/edu/narxoz/galactic/demo/Demo.java
@@ -0,0 +1,13 @@
+package edu.narxoz.galactic;
+import edu.narxoz.galactic.bodies.Planet;
+import edu.narxoz.galactic.cargo.Cargo;
+import edu.narxoz.galactic.drones.HeavyDrone;
+import edu.narxoz.galactic.drones.LightDrone;
+import edu.narxoz.galactic.dispatcher.Dispatcher;
+import edu.narxoz.galactic.task.DeliveryTask;
+import edu.narxoz.galactic.task.TaskState;
+public class Demo{
+    public static void main(String[] args){
+        
+    }
+}
